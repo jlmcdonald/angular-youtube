15e1321b3ebb5bac568943eb0ddefdf64431fbb9
Jarom McDonald <jlmcdonald@gmail.com>
2013-11-19 23:29:44 -0800

    Added support for playlists. If the listType parameter exists on the directive element, it will be used along with the list attribute (and the videoId will be ignored).

--- js/directives.js
+++ js/directives.js
@@ -2,15 +2,16 @@ angular.module('youtube.api.directives',[])
 .directive('youtube', ['youtubePlayer', function (YtPlayerApi) {
         return {
             restrict:'E',
-            link:function (scope, element,attrs) {
+            link:function (scope,element,attrs) {
                 YtPlayerApi.setPlayerId(attrs.id);
-		player_vars={};
-		allowed_vars=["autoplay","controls","html5"];
-		for (var idx in allowed_vars) {
-			if (allowed_vars[idx] in attrs) {
-				player_vars[allowed_vars[idx]]=attrs[allowed_vars[idx]];
-			}
-		}
+                player_vars={};
+                allowed_vars=["autoplay","controls","html5","listType","list"];
+                for (var idx in allowed_vars) {
+                    if (allowed_vars[idx] in attrs) {
+                        player_vars[allowed_vars[idx]]=attrs[allowed_vars[idx]];
+                    }
+                }
+                console.log(attrs['listtype']);
                 YtPlayerApi.setPlayerVars(player_vars);
                 YtPlayerApi.setVideoId(attrs.src);
             }
